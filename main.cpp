d29476a30b4adf6c3383711e70949b5506163a11
ApourtArtt <47370292+ApourtArtt@users.noreply.github.com>
2021-07-20 22:39:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,54 @@
-// Experimentations
 #include <stdint.h>
-#include <string>
 #include <vector>
 #include <map>
 #include <variant>
 
-#include <iostream>
+#include <asio.hpp> // !! asio dependency
+
+#include "Utils.h"
 
 enum class InfluxDbPrecision { ns, us, ms, s };
 
 class InfluxDbClient
 {
 public:
-    typedef std::variant< double, uint64_t, int64_t, std::string> FieldValue;
+    typedef std::variant<bool, double, uint64_t, int64_t, std::string> FieldValue;
 
-    InfluxDbClient(const std::string& Endpoint, const std::string& Token)
-        : token("Token " + Token)
+    InfluxDbClient(const std::string& Host, uint16_t Port, const std::string& Token, bool IsHttp)
+        : host(Host)
+        , port(std::to_string(Port))
+        , token(Token)
+        , isHttp(IsHttp)
+        , precision("ms")
+        , resolver(ioCtx)
+        , socket(ioCtx)
     {
-        url = Endpoint;
-        if (!url.ends_with("/"))
-            url += "/";
+        rebuildRequestSkeleton();
+
+        asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(host, port);
+        asio::connect(socket, endpoints);
     }
 
     // TODO: Send with a vector of all of this
-    void Send(const std::string& Measure, const std::map<std::string, std::string>& Tags, const std::map<std::string, FieldValue>& Fields, int64_t Timestamp = -1)
+    void Send(const std::string& Measure, const std::map<std::string, std::string>& Tags, const std::map<std::string, FieldValue>& Fields, int64_t Timestamp = 0)
     {
         std::string packet = getLine(Measure, Tags, Fields, Timestamp);
-        std::cout << packet;
+        if (isHttp)
+            sendHttp(packet);
     }
-    
+
     void SetOrganisation(const std::string& Organisation)
     {
         organisation = Organisation;
+        rebuildRequestSkeleton();
     }
-    
+
     void SetBucket(const std::string& Bucket)
     {
         bucket = Bucket;
+        rebuildRequestSkeleton();
     }
-    
+
     void SetPrecision(InfluxDbPrecision Precision)
     {
         if (Precision == InfluxDbPrecision::ns)
@@ -49,19 +59,31 @@ public:
             precision = "ms";
         else
             precision = "s";
+
+        rebuildRequestSkeleton();
     }
-  
+
 private:
+    void sendHttp(const std::string& Packet)
+    {
+        asio::streambuf request;
+        std::ostream request_stream(&request);
+        request_stream << req;
+        request_stream << "Content-Length: " << Packet.size() << "\r\n\r\n";
+        request_stream << Packet;
+        asio::write(socket, request);
+    }
+
     std::string getLine(const std::string& Measure, const std::map<std::string, std::string>& Tags, const std::map<std::string, FieldValue>& Fields, int64_t Timestamp)
     {
         std::string output;
         output += Measure;
-        
-        for (auto& tag: Tags)
+
+        for (auto& tag : Tags)
             output += "," + tag.first + "=" + tag.second;
-        
+
         output += " ";
-        
+
         auto&& it = Fields.begin();
 
         if (Fields.size() >= 1) [[likely]]
@@ -69,11 +91,11 @@ private:
             output += it->first + "=" + getFieldValue(it->second);
             std::advance(it, 1);
         }
-        
-        for (; it != Fields.end() ; it++)
+
+        for (; it != Fields.end(); it++)
             output += "," + it->first + "=" + getFieldValue(it->second);
 
-        if (Timestamp != -1) [[unlikely]]
+        if (Timestamp != 0) [[unlikely]]
             output += " " + std::to_string(Timestamp);
 
         return output;
@@ -82,37 +104,60 @@ private:
     template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
     std::string getFieldValue(const FieldValue& Value)
     {
-        return std::visit(overloaded {
-            //[](bool value) { if (value) return "true"; return "false"; },
+        return std::visit(overloaded{
+            [](bool value) -> std::string { return value ? "true" : "false"; },
             [](double value) { return std::to_string(value); },
             [](uint64_t value) { return std::to_string(value) + "u"; },
             [](int64_t value) { return std::to_string(value) + "i"; },
-            [](const std::string& value) { return value; },
+            [](const std::string& value) { return "\"" + ReplaceAll(value, "\"", "\\\"") + "\""; },
         }, Value);
     }
-    
+
+    void rebuildRequestSkeleton()
+    {
+        if (isHttp)
+            req = "POST http://" + host + ":" + port + "/api/v2/write?org=" + organisation + "&bucket=" + bucket + "&precision=" + precision + " HTTP/1.0\r\n";
+        else /* if udp */
+            req = ""; // TODO
+
+        if (!token.empty()) [[likely]]
+            req += "Authorization: Token " + token + "\r\n";
+
+        req += "Accept: */*\r\n";
+        req += "Content-Type: application/json; charset=utf-8\r\n";
+        req += "Connection: keep-alive\r\n";
+    }
+
+    std::string host;
+    std::string port;
     std::string token;
-    std::string url;
+    bool isHttp;
     std::string organisation;
     std::string bucket;
     std::string precision;
+
+    asio::io_context ioCtx;
+    asio::ip::tcp::resolver resolver;
+    asio::ip::tcp::socket socket;
+
+    std::string req;
 };
 
 int main(int, char**)
 {
-    InfluxDbClient client("http://127.0.0.1:8086/api/v2/", "token");
-    client.SetOrganisation("YOUR_ORG");
-    client.SetBucket("YOUR_BUCKET");
-    client.SetPrecision(InfluxDbPrecision::ms);
-    
-    client.Send("mem",
+    InfluxDbClient client("127.0.0.1", 8086, "Token", true);
+    client.SetOrganisation("Orga");
+    client.SetBucket("test");
+    client.SetPrecision(InfluxDbPrecision::s);
+
+    client.Send("NewMeasures",
         {
-            {"host", "host1"},
+            {"TagKey", "TagValue"},
         },
         {
-            {"used_percent", 23.43234543},
-            {"used_str", "\"fezfzefze\""},
-        },
-        1556896326
+            {"FieldKeyStr", "Field\"Value with quote in it"},
+            {"FieldKeyDouble", 3.14},
+            {"FielInt", -54},
+        }
     );
 }
