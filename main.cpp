c49aeea91a2b0714aac9077e1a12bb8cfee7ac30
ApourtArtt <47370292+ApourtArtt@users.noreply.github.com>
2021-07-21 21:39:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,28 +2,23 @@
 
 int main(int, char**)
 {
-    auto client = std::make_shared<InfluxDbClientAsync>("127.0.0.1", 8086, "Token");
-    // or auto client = std::make_shared<InfluxDbClient>("127.0.0.1", 8086, "Token");
+    auto client = std::make_shared<InfluxDbClientAsync>("127.0.0.1", 8086, "OusSA08-FUXiDc2ODjLlB6ciFPx1OEbAJ7EZk7-oipMh4rIe2kvs4zlMZS5-zZkGDfYOJVawWlIGr5CUU4OQhQ==");
     client->Connect();
     std::cout << client->IsValid();
-    client->SetOrganisation("Orga");
-    client->SetBucket("Bucket");
+    client->SetOrganisation("Ditz");
+    client->SetBucket("test");
     client->SetPrecision(InfluxDbPrecision::s);
     client->RebuildRequestSkeleton();
 
-    client->AddPacketToQueue("NewMeasure",
+    client->AddPacketToQueue("aaNewMeasures16",
         {
             {"TagKey", "TagValue"},
         },
         {
             {"FieldKeyStr", "Field\"Value with quote in it"},
             {"FieldKeyDouble", 3.14},
-            {"FieldInt", -54},
+            {"FielInt", -54},
         }
         );
     client->Send();
-    // If async :
-            Sleep(1000); // Wait a bit for the request to be processed
-            client->Close();
-    // If not async, nothing to do, since it's blocking
-}
+    Sleep(5000);
